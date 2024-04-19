25f461056a9a60319b4af7e6aff87ed42d20cb05
luigi052005 <luigilp05@gmx.de>
2024-04-20 00:43:46 +0200

    switched from !chat command to mentions

--- Bot.py
+++ Bot.py
@@ -12,7 +12,7 @@ MODEL = config.CONFIG["MODEL"]
 intents = discord.Intents.default()
 intents.typing = True
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents, heartbeat_timeout=120)
+bot = commands.Bot(command_prefix="!", intents=intents, heartbeat_timeout=240)
 
 pull_model(MODEL)
 
@@ -20,21 +20,29 @@ pull_model(MODEL)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.command(name="chat", description=f"Chat with {bot.user}")
-async def chat(ctx):
-    if ctx.author.name == bot.user.name:
+@bot.command(name="none", description=f"nothing")
+async def none(ctx):
+    pass
+
+@bot.event
+async def on_message(message):
+    mention = f'<@{bot.user.id}>'
+    if message.content.startswith(mention):
+        if message.author.name == bot.user.name:
+            return
+        if message.content == mention:
+            return
+
+        channel = message.channel
+        ctx = await bot.get_context(message)
+        message_history = []
+        await get_history(message_history, ctx, bot)
+
+        async with channel.typing():
+            response = generate_response(message_history)
+        await send_response(response, message)
+    else:
         return
-    if ctx.message.content == "!chat":
-        return
-
-    channel = ctx.channel
-    message_history = []
-    await get_history(message_history, ctx, bot)
-
-    async with channel.typing():
-        response = generate_response(message_history)
-    await send_response(response, ctx)
-
 
 
 bot.run(DISCORD_TOKEN)
--- history.py
+++ history.py
@@ -1,12 +1,13 @@
 import config
 from encodeimage import encode_image
 
-async def get_history(message_history, channel, bot):
-        async for message in channel.history(limit=10):
-            #add bot message to history
-            if message.author == bot.user:
-                message_history.append({'role': 'assistant', 'content': message.content})
-            #add user message to history
+async def get_history(message_history, ctx, bot):
+    async for message in ctx.history(limit=10):
+        #add bot message to history
+        if message.author == bot.user:
+            message_history.append({'role': 'assistant', 'content': message.content})
+        #add user message to history
+        if message.author != bot.user:
             if message.content[len('!chat'):] != "":
                 image_base64 = None
                 if message.attachments:
@@ -14,7 +15,7 @@ async def get_history(message_history, channel, bot):
                     image_base64 = await encode_image(image_base64, message, url)
                 message_history.append({'role': 'user', 'content': message.content[len('!chat'):], "images": ([image_base64] if image_base64 else [])})
 
-        system_message = {'role': 'system', 'content': config.CONFIG['SYSTEM']}
-        message_history.append(system_message)
-        message_history.reverse()
-        print(message_history)
+    system_message = {'role': 'system', 'content': config.CONFIG['SYSTEM']}
+    message_history.append(system_message)
+    message_history.reverse()
+    print(message_history)
