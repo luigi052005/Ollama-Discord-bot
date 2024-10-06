948be109290e3179ab92f5c43148f21c11d0adfc
luigi052005 <luigilp05@gmx.de>
2024-10-06 10:22:22 +0200

    Added Tool use

--- src/LLM/response_generator.py
+++ src/LLM/response_generator.py
@@ -1,16 +1,43 @@
 import ollama
 import config
+from src.Tools.tools import tools
+from src.Tools.get_weather import get_current_weather
 
 MODEL = config.MODEL
 NUM_PREDICT = config.NUM_PREDICT
 
-def generate_response(message_history):
+async def generate_response(message_history):
     while True:
+        # Initial chat response with Ollama
         response = ollama.chat(
             model=MODEL,
             messages=message_history,
+            tools=tools,
             options={
                 "num_predict": NUM_PREDICT
             }
         )
-        return response['message']['content']
+        if not response['message'].get('tool_calls'):
+            return response['message']['content']
+
+        # Process function calls made by the model
+        if response['message'].get('tool_calls'):
+            print("USING TOOLS")
+            available_functions = {
+                'get_current_weather': get_current_weather,
+            }
+            for tool in response['message']['tool_calls']:
+                function_to_call = available_functions[tool['function']['name']]
+                function_response = await function_to_call(tool['function']['arguments']['city'])
+                print(function_response)
+                # Add function response to the conversation
+                message_history.append(
+                    {
+                        'role': 'tool',
+                        'content': function_response,
+                    }
+                )
+
+                # Second API call: Get final response from the model
+            final_response = ollama.chat(model=MODEL, messages=message_history)
+            return final_response['message']['content']
