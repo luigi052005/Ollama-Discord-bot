8916331ad5cf72e37705b55e613e5fa28c436786
Luigi <101733154+luigi052005@users.noreply.github.com>
2025-02-26 19:42:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ IMAGE_MODEL = ""
 LOCAL_TIMEZONE = "Europe/Berlin"
 NUM_PREDICT = 2048
 HISTORY_LENGTH = 25
-SYSTEM = "Your name is Nyan and you are a Discord chatbot. You provide helpful and polite answers to user's questions."
+SYSTEM = "Your name is Bob and you are a Discord chatbot. You provide helpful and polite answers to user's questions."
